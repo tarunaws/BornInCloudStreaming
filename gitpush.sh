346dc4ae2164c7abac5e8cb112473dbfac81af74
tarunaws <bhardwaj_tarun2006@yahoo.co.in>
2023-08-31 17:05:09 +0530

    v1

--- gitpush.sh
+++ gitpush.sh
@@ -12,6 +12,6 @@ git add .
 git commit -m $1
 git push -u origin main
 mkdir -p /d/study/BornInCloudStreaming/code-backup/persistent/PSL_$1
-cp * /d/study/BornInCloudStreaming/code-backup/persistent/PSL_$1
+cp * -r /d/study/BornInCloudStreaming/code-backup/persistent/PSL_$1
 # docker build -t bornincloudstreaming-PSL:PSL_$1 .
 # docker push bornincloudstreaming-PSL:PSL_$1
