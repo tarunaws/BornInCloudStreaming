7441051c220efe03748f7299edafcbb0349032cf
tarunaws <bhardwaj_tarun2006@yahoo.co.in>
2023-08-31 17:03:42 +0530

    v1

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,17 @@
 #This is for pushing the content to github
 #!/bin/bash
+#Below commands will set up git account first time.
+#git config --global user.email "bhardwaj_tarun2006@yahoo.co.in"
+#git config --global user.name "tarunaws"
+#git config user.name
+# git remote add origin https://github.com/tarunaws/BornInCloudStreaming.git
+# git add .
+# git commit -m $1
+# git push -u origin main
 git add .
 git commit -m $1
 git push -u origin main
 mkdir -p /d/study/BornInCloudStreaming/code-backup/persistent/PSL_$1
 cp * /d/study/BornInCloudStreaming/code-backup/persistent/PSL_$1
-docker build -t bornincloudstreaming-PSL:PSL_$1 .
-docker push bornincloudstreaming-PSL:PSL_$1git
+# docker build -t bornincloudstreaming-PSL:PSL_$1 .
+# docker push bornincloudstreaming-PSL:PSL_$1
