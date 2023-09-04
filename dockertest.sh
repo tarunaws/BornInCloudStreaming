a6db406075cfd3e43556125613a000ffd103b6e9
tarunaws <bhardwaj_tarun2006@yahoo.co.in>
2023-09-04 15:29:02 +0530

    PSL_v1

--- dockertest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Run docker in remote server
-#ssh linuxtechi@192.168.10.10 bash -c "'echo $msg'"
-# docker build -t tarunaws/psl:$1 .
-# docker push tarunaws/psl:$1
-scp 1_copy.py 2_distribute.py 3_splitter.py 4_transcoder.py 5_joining.py 6_packager.py Dockerfile root@10.53.96.239:/home/lab/dockerbuild
-ssh root@10.53.96.239 docker build -t tarunaws/psl:something3 /home/lab/dockerbuild
