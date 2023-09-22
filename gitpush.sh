c722b6100dc0713da9b82a36057ecedc535195dc
tarunaws <bhardwaj_tarun2006@yahoo.co.in>
2023-09-22 15:17:07 +0530

    PSL_v1

--- gitpush.sh
+++ gitpush.sh
@@ -14,8 +14,8 @@ git commit -m $1
 git branch -M main
 #git push -u origin main
 git push origin main --force
-mkdir -p /d/study/BornInCloudStreaming/code-backup/persistent/$1
-cp * -r /d/study/BornInCloudStreaming/code-backup/persistent/$1
+mkdir -p /d/pslTranscoder/code-backup/persistent/$1
+cp * -r /d/pslTranscoder/code-backup/persistent/$1
 mkdir -p /d/"OneDrive - Persistent Systems Limited"/product/$1
 cp * -r /d/"OneDrive - Persistent Systems Limited"/product/$1
 scp 1_submit.py 2_contentid.py 3_splitter.py 4_transcoder.py 5_joining.py 6_packager.py Dockerfile root@10.53.96.239:/home/lab/dockerbuild
